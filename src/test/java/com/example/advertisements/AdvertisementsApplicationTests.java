d2c8a8975b36429f70d217573789cc44d8470f98
sergeynoskov <noskov-sa@ddsfirm.ru>
2023-08-14 10:49:38 +0500

    Initial commit

---
+++ src/test/java/com/example/advertisements/AdvertisementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advertisements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
